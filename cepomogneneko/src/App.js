25002f0e14449a8920695cb532b3de1f86644c44
Djole_Stamenkovic <djole.stamenkovic@arcadisgen.com>
2022-11-05 22:22:34 +0100

    djuka: Work on profile component

--- cepomogneneko/src/App.js
+++ cepomogneneko/src/App.js
@@ -3,22 +3,24 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Feed from "./components/Feed";
 import Profile from "./components/Profile";
+import { Container } from "@mui/material";
+import { Stack } from "@mui/system";
 
 function App() {
   return (
-    <>
+    <Container className="app" sx={{ display: "flex" }}>
       <BrowserRouter>
         <Navbar />
-        <div>
+        <Stack className="content-container">
           <Routes>
             <Route path="/" element={<Navigate to="/feed" />} />
             <Route path="/feed" element={<Feed />} />
             <Route path="/events" element={<h1>Main events</h1>} />
             <Route path="/profile" element={<Profile />} />
           </Routes>
-        </div>
+        </Stack>
       </BrowserRouter>
-    </>
+    </Container>
   );
 }
 

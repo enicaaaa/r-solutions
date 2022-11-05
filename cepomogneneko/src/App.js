8e814f689293ad220f16c731da0f4832a980e456
stefanodji <stefanodjihu@gmail.com>
2022-11-05 18:41:16 +0100

    djoks: MainEvent and CreateEvent

--- cepomogneneko/src/App.js
+++ cepomogneneko/src/App.js
@@ -1,13 +1,20 @@
 import "./App.scss";
 import Header from "./components/Navbar";
 import Login from "./components/Login.js";
+import MainEvent from "./components/MainEvent.js";
+import CreateMainEvent from "./components/CreateMainEvent.js";
 
 function App() {
   return (
     <>
       <Header />
       <Login />
-    </>
+      <MainEvent
+        nazivRadneAkcije="Ocistimo Nisavu"
+        lokacija="Nis"
+        decription="Ljudi, ajde da se skupimo i ocustimo ovu nasu reku :)"/>
+      <CreateMainEvent />
+    </> 
   );
 }
 

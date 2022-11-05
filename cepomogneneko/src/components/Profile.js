5eac89345d23d1c2b5cad38270af9e19465aa7b7
stefanodji <stefanodjihu@gmail.com>
2022-11-06 00:32:29 +0100

    djoks: User profile style added

--- cepomogneneko/src/components/Profile.js
+++ cepomogneneko/src/components/Profile.js
@@ -2,53 +2,42 @@ import "../styles/Profile.scss";
 // import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
 import { Avatar } from "@mui/material";
 import nature from "../assets/images/nature.jpeg";
 import Stack from "@mui/material/Stack";
+import "../styles/Profile.scss";
+import Typography from '@mui/material/Typography';
 
 const Profile = () => {
   return (
-//     <Stack
-//       // direction="row"
-//       sx={{ justifyContent: "center" }}
-//     >
-//       <Card sx={{ maxWidth: 345 }}>
-//         <CardActionArea>
-//           <Stack alignItems="center">
-//             <Avatar
-//               alt="Remy Sharp"
-//               src={nature}
-//               sx={{ width: 100, height: 100 }}
-//             />
-//           </Stack>
-//           <CardContent>
-//             <Typography gutterBottom variant="h5" component="div">
-//               Djordje Stamenkovic
-//             </Typography>
-//             <Typography variant="body2" color="text.secondary">
-//               Lizards are a widespread group of squamate reptiles, with over
-//               6,000 species, ranging across all continents except Antarctica
-//             </Typography>
-//           </CardContent>
-//         </CardActionArea>
-//       </Card>
-//     </Stack>
-//   );
-// };
-  <Card sx={{ maxWidth: "360px"}}>
-    <CardContent>
-      <Stack direction="column" alignItems="center">
-        <Avatar
-          alt="Remy Sharp"
-          src={nature}
-          sx={{ width: 100, height: 100 }}
-        />
-        <br/>
-      </Stack>
-    </CardContent>
-  </Card>
+    <div className="center-div-container">
+      <Card sx={{ width: "75%", minWidth: "320px"}}>
+        <CardContent>
+          <Stack direction="column" alignItems="center">
+            <Avatar
+              alt="Remy Sharp"
+              src={nature}
+              sx={{ width: 100, height: 100 }}
+            />
+            <br/>
+            <Typography sx={{ fontSize: "22px" }} color="text.primary" gutterBottom>
+              Kraljica Djubradi
+            </Typography>
+            <div className="div-divider-username"></div>
+            <Typography sx={{ fontSize: "18px" }} color="text.secondary" gutterBottom>
+              Mnogo volim djubre, pogotovo da ga bacam u smece :)
+            </Typography>
+            <div className="div-divider-descrption"></div>
+            <Typography sx={{ fontSize: "18px" }} color="text.secondary" gutterBottom>
+              Broj ciscenja: 3
+            </Typography>
+            <Typography sx={{ fontSize: "18px" }} color="text.secondary" gutterBottom>
+              Ukupan broj sati: 16h
+            </Typography>
+          </Stack>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
 export default Profile;

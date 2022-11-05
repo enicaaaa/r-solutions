59541b9e927f31ee03cb3f6fdb2e75194e1f813b
Djole_Stamenkovic <djole.stamenkovic@arcadisgen.com>
2022-11-05 23:46:12 +0100

    djuka: Style the navbar

--- cepomogneneko/src/App.js
+++ cepomogneneko/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Feed from "./components/Feed";
 import Profile from "./components/Profile";
 import MainEventList from "./components/MainEvent/MainEventList";
-import { Container } from "@mui/material";
+import { Button, Container } from "@mui/material";
 import { Stack } from "@mui/system";
 
 function App() {
@@ -12,6 +12,26 @@ function App() {
     <Container className="app" sx={{ display: "flex" }}>
       <BrowserRouter>
         <Navbar />
+        <Button
+          variant="contained"
+          className="create-user-event"
+          sx={{
+            fontSize: 20,
+            bgColor: "lightgreen",
+            color: "white",
+            border: "1px solid transparent",
+            ":hover": {
+              bgColor: "blue",
+              color: "lightgreen",
+              border: "1px solid white",
+            },
+            position: "absolute",
+            top: 80,
+          }}
+          size="large"
+        >
+          Prijavi mesto
+        </Button>
         <Stack className="content-container">
           <Routes>
             <Route path="/" element={<Navigate to="/feed" />} />
--- cepomogneneko/src/components/Navbar.js
+++ cepomogneneko/src/components/Navbar.js
@@ -22,54 +22,54 @@ import "../styles/Navbar.scss";
 
 // export default Navbar;
 
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import DeleteIcon from '@mui/icons-material/Delete';
-
-const pages = ['Glave objave', 'Objave korisnika'];
-const settings = ['Profile', 'Logout'];
+const pages = [
+  { name: "Objave korisnika", route: "/feed" },
+  { name: "Glavne objave", route: "/events" },
+];
+const settings = ["Profile", "Logout"];
 
 function Navbar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
-    console.log("Open Nav")
+    console.log("Open Nav");
   };
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
-    console.log("Open User")
+    console.log("Open User");
   };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
-    console.log("Close Nav")
+    console.log("Close Nav");
   };
 
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
-    console.log("Close User")
+    console.log("Close User");
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="absolute" sx={{ bgcolor: "black" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <DeleteIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
           <Typography
             variant="h6"
             noWrap
@@ -77,18 +77,19 @@ function Navbar() {
             href="/"
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.1rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              letterSpacing: ".1rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
-            ce pomognenEKO
+            ćepomognen
+            <Box className="navbar-eko">EKO</Box>?
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -103,28 +104,29 @@ function Navbar() {
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: { xs: "block", md: "none" },
               }}
             >
               {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
+                <Link to={page.route} key={page.name}>
+                  <MenuItem onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page.name}</Typography>
+                  </MenuItem>
+                </Link>
               ))}
             </Menu>
           </Box>
-          <DeleteIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
           <Typography
             variant="h5"
             noWrap
@@ -132,31 +134,25 @@ function Navbar() {
             href=""
             sx={{
               mr: 2,
-              display: { xs: 'flex', md: 'none' },
+              display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: 'monospace',
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.1rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              letterSpacing: ".1rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
-            ce pomognenEKO
+            ćepomognen
+            <Box className="navbar-eko">EKO</Box>?
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={() => {
-                  if(page == "'Glave objave'"){
-                    <Link to="/events"></Link>
-                  }
-                  
-                }}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
+              <Link to={page.route} key={page.name}>
+                <Button sx={{ my: 2, color: "white", display: "block" }}>
+                  {page.name}
+                </Button>
+              </Link>
             ))}
           </Box>
 
@@ -167,17 +163,17 @@ function Navbar() {
               </IconButton>
             </Tooltip>
             <Menu
-              sx={{ mt: '45px' }}
+              sx={{ mt: "45px" }}
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
@@ -195,4 +191,3 @@ function Navbar() {
   );
 }
 export default Navbar;
-

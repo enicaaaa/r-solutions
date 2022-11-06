73eef14b24d88f8ee91ba81b4b08a9184ef6616c
Djole_Stamenkovic <djole.stamenkovic@arcadisgen.com>
2022-11-06 03:02:30 +0100

    djuka: Style UserEvent and add util classes

--- cepomogneneko/src/components/UserEvent/UserEvent.js
+++ cepomogneneko/src/components/UserEvent/UserEvent.js
@@ -1,31 +1,32 @@
 import { Title } from "@mui/icons-material";
 import { Card } from "@mui/material";
-import { Box } from "@mui/system";
+import { Box, Stack } from "@mui/system";
 import nature from "../../assets/images/nature.jpeg";
 import Brightness1Icon from "@mui/icons-material/Brightness1";
+import { Link } from "react-router-dom";
 
 const UserEvent = () => {
   return (
-    <>
-      <Box className="user-event__in-progress">
-        <Brightness1Icon sx={{ color: "red" }} />
-        in Progress
-      </Box>
-      <Card className="user-event">
-        <div className="user-event__info-container">
+    <Stack className="user-event u-border-radius-20">
+      <Link to="/feed/1" className="u-text-decoration-none">
+        <Box className="user-event__in-progress">
+          <Brightness1Icon sx={{ color: "red" }} />
+          Čišćenje u toku
+        </Box>
+        <Card className="user-event__info-container u-border-radius-20">
           <img className="user-event__image" src={nature} alt="nature" />
-          <div className="user-event__event-info">
-            <div className="user-event__user-name">
-              Created by: Djordje Stamenkovic
-            </div>
-            <div className="user-event__event-location">
-              Location: Nis, Serbia
-            </div>
-            <div className="user-event__event-timer">Timer: 14 min</div>
-          </div>
-        </div>
-      </Card>
-    </>
+          <Stack className="user-event__event-info">
+            <Box className="user-event__user-name">
+              Postavio/la: Djordje Stamenkovic
+            </Box>
+            <Box className="user-event__event-location">
+              Lokacija: Nis, Serbia
+            </Box>
+            <Box className="user-event__event-timer">Tajmer: 14 min</Box>
+          </Stack>
+        </Card>
+      </Link>
+    </Stack>
   );
 };
 
